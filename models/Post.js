1ae182183acefbf35dedb10ac375a58e3551ca19
cjabe <adrinbershik.2201008@srec.ac.in>
2025-07-05 13:00:45 +0530

    fixed caption required issue

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    caption: { type: String, required: true },
+    caption: { type: String },
     images: [
       {
         type: String,
